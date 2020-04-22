76002e99fd8d4347d4877881dded62bdc1d3e126
Douglas Cardoso <douglas.dcm@gmail.com>
2020-04-22 19:27:07 -0300

    fisrt

---
+++ Catalog.Api/Models/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+            public TodoContext(DbContextOptions options )
+                : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+        
+    }
+}
